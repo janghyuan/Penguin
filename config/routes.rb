8c4850c0e3394e8ca6bf30d313847a905e01a8b9
janghyuan <janghuan@gmail.com>
2016-09-04 20:26:08 +0800

    add common title and home page, change root url to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+	get 'pages/home'
   get 'pages/about'
   get 'pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # root 'application#hello'
+  root 'pages#home'
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+	def setup
+		@title = "Shandong University of Technology"
+	end
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@title}"
   end
 
   test "should get help" do
   	get pages_help_url
   	assert_response :success
+    assert_select "title", "Help | #{@title}"
+  end
+
+  test "should get home" do
+  	get pages_home_url
+  	assert_response :success
+  	assert_select "title", "Home | #{@title}"
   end
 end
