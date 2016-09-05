1f050dbb60a27fd80adf5a6db9cda549e495ff05
janghyuan <janghuan@gmail.com>
2016-09-05 22:18:14 +0800

    add users#new and change its route to signup_url

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   root 'pages#home'
+
+  # 用户注册
+  get 'signup', to: 'users#new'
 end
