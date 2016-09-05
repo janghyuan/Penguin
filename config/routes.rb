1a82d08f10ddf966a6cae49d6cb5cafcffce0be8
janghyuan <janghuan@gmail.com>
2016-09-05 18:27:33 +0800

    add named routes like about_path instead of pages_about_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 	get 'pages/home'
   get 'pages/about'
   get 'pages/help'
+  get 'home', to: 'pages#home'
+  get 'help', to: 'pages#help'
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,19 +5,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
 		@title = "Shandong University of Technology"
 	end
   test "should get about" do
-    get pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@title}"
   end
 
   test "should get help" do
-  	get pages_help_url
+  	get help_url
   	assert_response :success
     assert_select "title", "Help | #{@title}"
   end
 
   test "should get home" do
-  	get pages_home_url
+  	get home_url
   	assert_response :success
   	assert_select "title", "#{@title}"
   end
