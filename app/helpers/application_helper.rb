31a45f0246f2ff5e74f36680fa6b3b34622d26f4
janghyuan <janghuan@gmail.com>
2016-09-07 08:48:55 +0800

    add gravatar_for helper and user profile card template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
 			"#{page_title} | #{base_title}"
 		end
 	end
+	def gravatar_for user, options = {}
+		size = options[:size] || 64
+		style = options[:class] || ''
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar #{style}")
+	end
 end
