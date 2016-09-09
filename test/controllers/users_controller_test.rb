862c94e90a14342031a59dd1e9c754b0e791c114
janghyuan <janghuan@gmail.com>
2016-09-09 22:58:53 +0800

    add users page and only logged_in users can see it

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -22,4 +22,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   	assert_not flash.empty?
   	assert_redirected_to root_url
   end
+  test "should redirect index when not logged in" do
+    get users_url
+    assert_redirected_to login_url
+  end
 end
